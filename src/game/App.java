1086c30e024a3b5d605a1cb7cb96ab436d6955d6
sipriano183 <77734544+sipriano183@users.noreply.github.com>
2021-01-28 16:27:55 -0300

    Clear Screen Implement

--- src/game/App.java
+++ src/game/App.java
@@ -1,10 +1,12 @@
 package game;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import chess.exceptions.ChessException;
 
 public class App {
 
@@ -16,15 +18,26 @@ public class App {
 				
 				// This while loop keeps the program running and executes the moves according to the source and target
 				while (true) {
-					UI.printBoard(chessMatch.getPieces());
-					System.out.println();
-					System.out.print("Source: ");
-					ChessPosition source = UI.readChessPosition(sc);
-					System.out.println();
-					System.out.print("Target: ");
-					ChessPosition target = UI.readChessPosition(sc);
-					
-					ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+					try {
+						UI.clearScreen();
+						UI.printBoard(chessMatch.getPieces());
+						System.out.println();
+						System.out.print("Source: ");
+						ChessPosition source = UI.readChessPosition(sc);
+						System.out.println();
+						System.out.print("Target: ");
+						ChessPosition target = UI.readChessPosition(sc);
+						
+						ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+					}
+					catch (ChessException e) {
+						System.out.println(e.getMessage());
+						sc.nextLine();
+					}
+					catch (InputMismatchException e) {
+						System.out.println(e.getMessage());
+						sc.nextLine();
+					}
 				}
 
 	}
