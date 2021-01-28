4e30fc15ce7f328d59d78991c62fb6e0684b23dc
sipriano183 <77734544+sipriano183@users.noreply.github.com>
2021-01-28 11:09:26 -0300

    Initial Board and Piece implementation

--- src/board/Board.java
+++ src/board/Board.java
@@ -2,12 +2,16 @@ package board;
 
 public class Board {
 	
+	// A board will have rows and columns and many pieces
 	private Integer rows;
 	private Integer columns;
+	private Piece[][] pieces;
 	
+	// When the board is initiated it will set a new piece in the row and column set in the constructor
 	public Board(Integer rows, Integer columns) {
 		this.rows = rows;
 		this.columns = columns;
+		pieces = new Piece[rows][columns];
 	}
 
 	public Integer getRows() {
@@ -24,10 +28,6 @@ public class Board {
 
 	public void setColumns(Integer columns) {
 		this.columns = columns;
-	}
-	
-	public Piece piece(int row, int column) {
-		
 	}
 	
 	
--- src/board/Piece.java
+++ src/board/Piece.java
@@ -1,5 +1,21 @@
 package board;
 
 public class Piece {
+	
+	// Initial position should not be visible by the game layer.
+	protected Position position;
+	private Board board;
+	
+	// Initial position will be null
+	public Piece(Board board) {
+		this.board = board;
+	}
+
+	// Get method should not be visible by the game layer since its for internal use
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
 
 }
