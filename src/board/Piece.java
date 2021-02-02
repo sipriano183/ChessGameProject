e41017fec2d1f09d8fbb5b5a6503e08246079622
sipriano183 <77734544+sipriano183@users.noreply.github.com>
2021-02-02 15:17:38 -0300

    Initial piece move implement

--- src/board/Piece.java
+++ src/board/Piece.java
@@ -1,11 +1,11 @@
 package board;
 
-public class Piece {
-	
+public abstract class Piece {
+
 	// Initial position should not be visible by the game layer.
 	protected Position position;
 	private Board board;
-	
+
 	// Initial position will be null
 	public Piece(Board board) {
 		this.board = board;
@@ -15,6 +15,29 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-	
+
+	// This abstract method will be used by the pieces to make the possible moves
+	// logic
+	public abstract boolean[][] possibleMoves();
+
+	// This method returns the abstract method at the given position's row and
+	// column
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	// This method will read the matrix and check if there are any possible moves.
+	// It does that by comparing the given position row and column and returning a boolean type
+	public boolean isThereAnyPossibleMoves() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
 }
